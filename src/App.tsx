de8d2e4085bb601039764ca8d749aaafc79a68e6
NguyenNhuDai05 <nguyennhudai05@gmail.com>
2025-10-10 01:35:53 +0700

    feat(register-page):basic register layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
+import React from "react"
+import RegisterPage from "@/pages/RegisterPage"
 
 function App() {
-
-
   return (
-    <>
-      
-    </>
-  );
+    <RegisterPage />
+  )
 }
 
-export default App;
+export default App
